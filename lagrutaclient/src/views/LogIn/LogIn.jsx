34980e3abe65c0169047ced591192367c3977fe4
sofiaparraweb <sofiaparraweb@gmail.com>
2023-06-14 20:33:00 -0300

    mergeo sofi gonza

--- lagrutaclient/src/views/LogIn/LogIn.jsx
+++ lagrutaclient/src/views/LogIn/LogIn.jsx
@@ -1,1 +1,20 @@
-<p className="lala"></p>
+// Componente LogIn
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LogIn = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = () => {
+    loginWithRedirect();
+  };
+
+  return (
+    <div>
+      <h2>Iniciar sesión</h2>
+      <button onClick={handleLogin}>Iniciar sesión con Auth0</button>
+    </div>
+  );
+};
+
+export default LogIn;
