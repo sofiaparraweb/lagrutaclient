29c9f1d404104bfb6f50fc68d1770561eb1768ec
GonzaBerdazaiz <gonzaloberdazaiz@hotmail.com>
2023-06-13 21:04:24 -0300

    Ruta Tienda

--- lagrutaclient/src/App.jsx
+++ lagrutaclient/src/App.jsx
@@ -5,12 +5,14 @@ import "./App.css";
 
 /* Componentes */
 import News from "./views/Noticias/News";
+import Tienda from "./views/Tienda/Tienda";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/Noticias" element={<News />} />
+        <Route path="/Tienda" element={<Tienda />} />
       </Routes>
     </div>
   );
