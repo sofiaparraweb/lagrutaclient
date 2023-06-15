eb31d4a3884251743a9968829830b2db7734287c
sofiaparraweb <sofiaparraweb@gmail.com>
2023-06-15 09:38:55 -0300

    Guardando cambios antes de cambiar de rama

--- lagrutaclient/src/App.jsx
+++ lagrutaclient/src/App.jsx
@@ -1,34 +1,36 @@
-// App.jsx
+// App.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Auth0Provider } from "@auth0/auth0-react";
-import dotenv from "dotenv";
-
-/* Cargar variables de entorno desde .env */
-dotenv.config();
+import { useAuth0 } from "@auth0/auth0-react";
 
 /* Componentes */
-import Home from "./views/Home/Home"
+import Home from "./views/Home/Home";
 import News from "./views/Noticias/News";
-import LogIn from "./views/LogIn/LogIn";
 import Tienda from "./views/Tienda/Tienda";
+import Perfil from "./views/Perfil/Perfil";
+import LogIn from "./views/LogIn/LogIn";
+import LogOut from "./views/LogIn/LogOut";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
-  const auth0Config = {
-      domain: import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN,
-      clientId: import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID,
-  };
-  
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
   return (
     <div className="App">
-<Auth0Provider {...auth0Config}>
-  <Routes>
+      <NavBar isAuthenticated={isAuthenticated} />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Noticias" element={<News />} />
         <Route path="/Tienda" element={<Tienda />} />
+        <Route path="/Perfil" element={<Perfil />} />
+        <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/LogOut" element={<LogOut />} />
       </Routes>
-      </Auth0Provider>
     </div>
   );
 };
--- lagrutaclient/src/components/NavBar/NavBar.jsx
+++ lagrutaclient/src/components/NavBar/NavBar.jsx
@@ -1,38 +1,63 @@
+// NavBar.js
+import React from "react";
 import { Link } from "react-router-dom";
 import style from "./NavBar.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const NavBar = () => {
+const NavBar = ({ isAuthenticated }) => {
+  const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({ returnTo: window.location.origin });
+  };
 
   return (
     <nav className={style.navContainer}>
-        <div className={style.LeftContainer}>
-          <Link to="/" className={style.link}>
-            LOGO!!!
+      <div className={style.LeftContainer}>
+        <Link to="/" className={style.link}>
+          LOGO!!!
+        </Link>
+        <Link to="/about" className={style.link}>
+          About
+        </Link>
+      </div>
+      <div className={style.dropdownContainer}>
+        <Link to="/Conocenos" className={style.link}>
+          Ayuda Hoy
+        </Link>
+        <div className={style.dropdownContent}>
+          <Link to="/Dona" className={style.dropdownOption}>
+            Dona
           </Link>
-          <Link to="/about" className={style.link}> 
-            About
+          <Link to="/SePadrino" className={style.dropdownOption}>
+            Se padrino
+          </Link>
+          <Link to="/SeVoluntario" className={style.dropdownOption}>
+            Se voluntario
           </Link>
         </div>
-        <div className={style.dropdownContainer}>
-            <Link to="/Conocenos" className={style.link}>
-              Ayuda Hoy
+      </div>
+      <div className={style.rightSection}>
+        <Link to="/Tienda" className={style.link}>
+          Tienda
+        </Link>
+        {isAuthenticated ? (
+          <>
+            <Link to="/Perfil" className={style.link}>
+              Perfil
             </Link>
-            <div className={style.dropdownContent}>
-              <Link to="/Dona" className={style.dropdownOption}>Dona</Link>
-              <Link to="/SePadrino" className={style.dropdownOption}>Se padrino</Link>
-              <Link to="/SeVoluntario" className={style.dropdownOption}>Se voluntario</Link>
-            </div>
-          </div>
-        <div className={style.rightSection}>
-            <Link to="/Tienda" className={style.link}>
-              Tienda
-            </Link>
-            <Link to="/Login" className={style.link}>
-              Inicia sesión
-            </Link>
-          </div>
+            <button onClick={handleLogout} className={style.link}>
+              Cerrar Sesión
+            </button>
+          </>
+        ) : (
+          <Link to="/LogIn" className={style.link}>
+            Inicia sesión
+          </Link>
+        )}
+      </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
