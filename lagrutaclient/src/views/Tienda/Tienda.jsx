a691d6a80f00fdf9c3c7d34eca49214a1aa2b80f
GonzaBerdazaiz <gonzaloberdazaiz@hotmail.com>
2023-06-14 22:58:32 -0300

    Cambios en Tienda

--- lagrutaclient/src/views/Tienda/Tienda.jsx
+++ lagrutaclient/src/views/Tienda/Tienda.jsx
@@ -1,13 +1,29 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProducts } from '../../Redux/actions';
+//import Carrito from "../../components/Carrito/Carrito";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
+import TiendaItemsContenedor from "../../components/TiendaItemsContenedor/TiendaItemsContenedor";
 import style from "./Tienda.module.css";
 
 const Tienda = () => {
+
+  const dispatch = useDispatch();
+  const allProducts = useSelector(state => state.allProducts);
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, [dispatch]);
+
   return (
     <div>
       <NavBar />
       <div>
         <h1 className={style.tienda}>Tiendaaaaaaaa</h1>
+      </div>
+      <div>
+        <TiendaItemsContenedor products={allProducts}/>
       </div>
       <Footer />
     </div>
@@ -15,30 +31,3 @@ const Tienda = () => {
 }
 
 export default Tienda;
-
-
-// //import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// //import { getAllProducts } from '../../Redux/actions';
-// import TiendaItemsContenedor from "../../components/TiendaItems/TiendaItemsContenedor";
-// import Carrito from "../../components/Carrito/Carrito";
-
-// const Tienda = () => {
-//   //const dispatch = useDispatch();
-//   const allProducts = useSelector(state => state.allProducts);
-
-// //   useEffect(() => {
-// //     dispatch(getAllProducts());
-// //   }, [dispatch]);
-
-//   return (
-//     <div>
-//         <h2>Tienda</h2>
-//         <div>
-//             <TiendaItemsContenedor products={allProducts}/>
-//         </div>
-//         <Carrito />
-//         
-//     </div>
-//   );
-// };
