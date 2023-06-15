c30cb689628c7ba6d4aff6e0f1c2a72cdb4a60f6
sofiaparraweb <sofiaparraweb@gmail.com>
2023-06-14 21:25:42 -0300

    demo basica

--- lagrutaclient/src/views/LogIn/LogIn.jsx
+++ lagrutaclient/src/views/LogIn/LogIn.jsx
@@ -1,18 +1,13 @@
-// Componente LogIn
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import './LogIn.css'; // Importa tu archivo CSS para estilos específicos de LogIn
 
 const LogIn = () => {
   const { loginWithRedirect } = useAuth0();
 
-  const handleLogin = () => {
-    loginWithRedirect();
-  };
-
   return (
-    <div>
-      <h2>Iniciar sesión</h2>
-      <button onClick={handleLogin}>Iniciar sesión con Auth0</button>
+    <div className="login-container"> 
+      <button onClick={loginWithRedirect}>Iniciar sesión</button>
     </div>
   );
 };
