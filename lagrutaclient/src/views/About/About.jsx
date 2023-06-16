2a67ab3caf5cd96eb5f26373154d19a0206f410d
GonzaBerdazaiz <gonzaloberdazaiz@hotmail.com>
2023-06-16 18:03:32 -0300

    Cambios en tienda

--- lagrutaclient/src/views/About/About.jsx
+++ lagrutaclient/src/views/About/About.jsx
@@ -11,7 +11,7 @@ const About = () =>{
             <p> Hoy, después de tantos años, podemos seguir disfrutando de este lugar que tanto nos enseña, y con orgullo podemos contar que contamos con diferentes programas como ser, un comedor infantil, talleres de niños, jóvenes y mujeres. Trabajamos con la comunidad y con su salud.</p>
             <section className={style.parallaxContent}>
                 <div className={style.parallax} />
-                <h1 className={style.Tittle}> MISION </h1>
+                <h1 className={style.Tittle} style={{ backgroundColor:'rgba(223,184,55,0.8)' }}> MISION </h1>
             </section>
             <div className={style.Parrafos} style={{ backgroundColor:'rgba(223,184,55,255)' }}>
                 <p>Queremos ser un lugar de encuentro, acogida y desarrollo integral de las personas y la comunidad de Barrio Ampliacion los Vallistos.</p>
