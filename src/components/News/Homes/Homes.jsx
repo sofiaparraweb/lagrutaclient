7047218f671efad7ecee761fa82cb16bac59fa64
fedezelarayan <federico.zelarayan@hotmail.com>
2023-06-26 23:12:52 -0300

    implementacion de paginado en seccion noticias

--- src/components/News/Homes/Homes.jsx
+++ src/components/News/Homes/Homes.jsx
@@ -1,16 +1,33 @@
-import React from "react"
+import { ppost } from "../../../dummyData";
+import React, { useState }from "react"
 import Side from "../SideBar/side/Side.jsx";
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import NewsCarrousel from "./NewsCarrousel/NewsCarrousel.jsx";
 import Paginado from "./CardNews/Paginado/Paginado.jsx";
-
 import CardNews from "./CardNews/CardNews.jsx";
-
 import style from "./style.module.css"
 
 
+
 const Homes = () => {
+
+  const [currentPage, setCurrentPage] = useState(1); 
+  const noticesPerPage = 4;
+
+  const notices = ppost;
+
+  const handlePaginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };  
+  
+  const indexOfLastProduct = currentPage * noticesPerPage; 
+  const indexOfFirstProduct = indexOfLastProduct - noticesPerPage;
+  const currentNotices = notices?.length > 0 && notices.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  
   return (
     <>
       <div className={style.mainNews}>
@@ -18,8 +35,12 @@ const Homes = () => {
           <section className={style.mainContent}>
            <NewsCarrousel />
            <hr />
-           <Paginado />
-           <CardNews />
+           <Paginado 
+           noticesPerPage = {noticesPerPage}
+           allNotices={notices?.length}
+           currentPage = {currentPage}
+           handlePaginate = {handlePaginate}/>
+           <CardNews notices = {currentNotices}/>
          {/* aqui van componentes de relleno, actualmente desactivados*/}
           </section>
           <section className={style.sideContent}>
