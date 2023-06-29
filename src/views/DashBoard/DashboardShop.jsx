ff71456a8dfde577414df8e03b09d6713f240572
GonzaBerdazaiz <gonzaloberdazaiz@hotmail.com>
2023-06-29 20:59:50 -0300

    Mapa en home y dashboard tienda

--- src/views/DashBoard/DashboardShop.jsx
+++ src/views/DashBoard/DashboardShop.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import FormCreacion from "../../components/DashBoard/Tienda/FormCreacion";
 import SideDash from "../../components/DashBoard/sideDash/sideDash.jsx";
 
 export default function DashboardShop() {
   return (
     <>
       <SideDash />
+      <FormCreacion /> 
     </>
   );
 }
