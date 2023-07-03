6deebc70e7f9672e0fbe373aae77e3be08e9be45
Tobami <malenaparaschuk@gmail.com>
2023-07-03 20:57:12 +0200

    modifique index.html

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,17 +8,22 @@ import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm();
-    const dispatch = useDispatch();
-  
-    const onSubmit = (data) => {
-      dispatch(formFooter(data.email));
-      reset();
-      Swal.fire({
-        icon: 'success',
-        title: '¡Formulario enviado!',
-      });
-    };    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    dispatch(formFooter(data.email));
+    reset();
+    Swal.fire({
+      icon: "success",
+      title: "¡Formulario enviado!",
+    });
+  };
 
   return (
     <footer className={style.mainFooter}>
@@ -26,7 +31,7 @@ const Footer = () => {
         <div className={style.footerSection}>
           <h3 className={style.title}>Conocé LA GRUTA</h3>
           <ul>
-            <li className={style.itemsTex} >
+            <li className={style.itemsTex}>
               <a href="/about">Qué hace la Gruta</a>
             </li>
             <li className={style.itemsTex}>
@@ -54,8 +59,8 @@ const Footer = () => {
         </div>
 
         <form className={style.footerSection} onSubmit={handleSubmit(onSubmit)}>
- <h3 className={style.title}>¡Suscríbete para más información!</h3>
-   <input
+          <h3 className={style.title}>¡Suscríbete para más información!</h3>
+          <input
             className={style.input}
             type="email"
             name="email"
@@ -65,21 +70,26 @@ const Footer = () => {
               pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
             })}
           />
-          {errors.email && <p className={style.fail}>Correo electrónico no válido</p>}
+          {errors.email && (
+            <p className={style.fail}>Correo electrónico no válido</p>
+          )}
           <button className={style.btnSuscripcion} type="submit">
             SUSCRIBIRSE
           </button>
         </form>
 
         <div className={style.footerSection}>
-        <div className={style.footerSection}>
-      <Link to="/donacion" className={style.btnFooter}>
-        DONÁ AHORA
-      </Link>
-    </div>
-          <ul className={style.redes} >
-            <li className={style.itemsRedes} >
-              <a href="https://www.facebook.com/lagrutacomedor" target="_blank" rel="noreferrer">
+          <div className={style.footerSection}>
+            <Link to="/donacion" className={style.btnFooter}>
+              DONÁ AHORA
+            </Link>
+          </div>
+          <ul className={style.redes}>
+            <li className={style.itemsRedes}>
+              <a
+                href="https://www.facebook.com/lagrutacomedor"
+                target="_blank"
+                rel="noreferrer">
                 <FaFacebook />
               </a>
             </li>
@@ -89,12 +99,18 @@ const Footer = () => {
               </a>
             </li>
             <li className={style.itemsRedes}>
-              <a href="https://www.instagram.com/lagrutacdi/" target="_blank" rel="noreferrer">
+              <a
+                href="https://www.instagram.com/lagrutacdi/"
+                target="_blank"
+                rel="noreferrer">
                 <FaInstagram />
               </a>
             </li>
             <li className={style.itemsRedes}>
-              <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+              <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noreferrer">
                 <FaYoutube />
               </a>
             </li>
@@ -107,6 +123,6 @@ const Footer = () => {
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
