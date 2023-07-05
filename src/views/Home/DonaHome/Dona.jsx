24eba80170080c09f409bed809a95b4d5477f293
sofiaparraweb <sofiaparraweb@gmail.com>
2023-07-05 19:37:23 -0400

    estilos

--- src/views/Home/DonaHome/Dona.jsx
+++ src/views/Home/DonaHome/Dona.jsx
@@ -12,7 +12,8 @@ const DonaHome = () => {
       <p id="PrimerAmarillo">
         En La Gruta queremos devolver la alegría y la espontaneidad a los niños.
         Queremos que jueguen, que aprendan y que se rían, que se sientan queridos,
-        que abracen, que bailen, que inventen, que admiren. Queremos que los niños sean niños
+        que abracen, que bailen, que inventen, que admiren. 
+        Queremos que los niños sean niños
       </p>
       <p id="SegundoAmarillo">
         Gracias a los contribuyentes, vamos creciendo cada vez más. Tu aporte permite
